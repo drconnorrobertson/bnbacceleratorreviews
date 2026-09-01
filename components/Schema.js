2c38a3d5060452955b730b495ee9c65803fe1cfd
drconnorrobertson <dr.connor.robertson@gmail.com>
2026-09-01 11:09:49 -0400

    Add components/Schema.js

---
+++ components/Schema.js
@@ -0,0 +1,14 @@
+export default function Schema({ data, type }) {
+  const schemaData = {
+    "@context": "https://schema.org",
+    "@type": type,
+    ...data,
+  };
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
+    />
+  );
+}
