45308d2cdb3ab99b51d5d7b17162921c6bea7cc1
drconnorrobertson <dr.connor.robertson@gmail.com>
2026-09-01 11:16:42 -0400

    Add app/reviews/page.js

---
+++ app/reviews/page.js
@@ -0,0 +1,5 @@
+import { reviews } from "@/data/reviews";
+import ReviewCard from "@/components/ReviewCard";
+import Schema from "@/components/Schema";
+import StarRating from "@/components/StarRating";
+import Link from "next/link";
